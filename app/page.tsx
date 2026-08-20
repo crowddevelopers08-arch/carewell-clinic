a378ec29adc6236a8383269407030e6516b0f88f
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-08-20 17:36:55 +0530

    carewall lp code atteched

--- app/page.tsx
+++ app/page.tsx
@@ -11,23 +11,40 @@ import ContactMapSection from "@/components/ContactMapSection";
 import Footer from "@/components/Footer";
 import { MobileActionBar } from "@/components/MobileActionBar";
 import ScientificHealthcareHero from "@/components/ScientificHealthcareHero";
+import Script from "next/script";
 
 export default function Home() {
+  return (
+    <>
+      {/* Google Ads (gtag.js) - using Next.js Script component */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18400739132"
+      />
+      <Script
+        id="google-ads-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18400739132');
+          `,
+        }}
+      />
 
-  return(
-  <>
-  <ScientificHealthcareHero />
-  <BeforeAfter />
-  <AboutDental />
-  <DentalServices />
-  <RootCanalJourney />
-  <RoadmapInfographic />
-  <WelcomingBanner />
-  <FrequentlyAskedQuestions />
-  <ContactMapSection />
-  <Footer />
-  <MobileActionBar />
-
-  </>
+      <ScientificHealthcareHero />
+      <BeforeAfter />
+      <AboutDental />
+      <DentalServices />
+      <RootCanalJourney />
+      <RoadmapInfographic />
+      <WelcomingBanner />
+      <FrequentlyAskedQuestions />
+      <ContactMapSection />
+      <Footer />
+      <MobileActionBar />
+    </>
   );
 }
